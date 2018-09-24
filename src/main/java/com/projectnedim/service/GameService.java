aed0f49fc4b5c79fd04a12ae732407e9c114c20c
Kulovac Nedim <nedim.kulovac@gmail.com>
2018-09-24 17:31:44 +0200

    Servlet added. Minor changes on model layer

---
+++ src/main/java/com/projectnedim/service/GameService.java
@@ -0,0 +1,20 @@
+package main.java.com.projectnedim.service;
+
+import main.java.com.projectnedim.model.Game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class GameService {
+
+    public List<Game> getSimulatedGames(int skillLevel, int numberOfGames) {
+
+        List<Game> games = new ArrayList<>();
+
+        // implement
+
+        return games;
+    }
+
+}
